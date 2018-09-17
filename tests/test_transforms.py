7e14fcfa4d0e96cabc0701b5cd600f014251a4a0
Randrian <Randrian@localhost>
2018-09-17 15:44:19 +0200

    ! test: added matplotlib.use('agg') to fix pipelines on bitbucket

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,3 +1,7 @@
+# to prevent matplotlib from trying to import tkinter
+import matplotlib
+matplotlib.use('agg')
+
 import unittest
 import numpy as np
 import sys
@@ -25,12 +29,11 @@ while True:
     else:
         break
 
-#print("path", os.path.dirname(__file__))
-#print("path", os.getcwd())
 sys.path.insert(0, os.path.dirname(__file__))
 import strategies as ct_st
 
 points = st_np.arrays(dtype="float", shape=st.tuples(st.integers(1, 100), st.integers(2, 2)), elements=st.floats(0, 2448))
+
 
 class TempFile:
     def __enter__(self):
