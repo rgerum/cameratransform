e7c4f143a22c500d9656644a2a64b682e14d3a79
Randrian <richard.gerum@yahoo.de>
2018-11-21 15:14:00 +0100

    * camera: moved intersectionOfTwoLines to ray.py

--- CameraTransform/camera.py
+++ CameraTransform/camera.py
@@ -11,6 +11,7 @@ from .projection import RectilinearProjection, CameraProjection
 from .spatial import SpatialOrientation
 from .lens_distortion import NoDistortion, LensDistortion
 from . import gps
+from . import ray
 
 
 def _getSensorFromDatabase(model):
@@ -201,70 +202,15 @@ class CameraGroup(ClassWithParameterSet):
             self.parameters.parameters[name].set_stats(np.mean(data), np.std(data))
         return fit_data
 
-    def intersectionOfTwoLines(self, p1, v1, p2, v2):
-        """
-        Get the point closest to the intersection of two lines. The lines are given by one point (p1 and p2) and a
-        direction vector v (v1 and v2). If a batch should be processed, the multiple direction vectors can be given.
-        """
-        # if we transform multiple points in one go
-        if len(v1.shape) == 2:
-            a1 = np.einsum('ij,ij->i', v1, v1)
-            a2 = np.einsum('ij,ij->i', v1, v2)
-            b1 = -np.einsum('ij,ij->i', v2, v1)
-            b2 = -np.einsum('ij,ij->i', v2, v2)
-            c1 = -np.einsum('ij,j->i', v1, p1 - p2)
-            c2 = -np.einsum('ij,j->i', v2, p1 - p2)
-            res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]).transpose(2, 0, 1), np.array([c1, c2]).T)
-            res = res[:, None, :]
-            return np.mean([p1 + res[..., 0] * v1, p2 + res[..., 1] * v2], axis=0)
-        else:  # or just one point
-            a1 = np.dot(v1, v1)
-            a2 = np.dot(v1, v2)
-            b1 = -np.dot(v2, v1)
-            b2 = -np.dot(v2, v2)
-            c1 = -np.dot(v1, p1 - p2)
-            c2 = -np.dot(v2, p1 - p2)
-            res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]), np.array([c1, c2]))
-            res = res[None, None, :]
-            return np.mean([p1 + res[..., 0] * v1, p2 + res[..., 1] * v2], axis=0)[0]
-
     def spaceFromImages(self, points1, points2):
         p1, v1 = self.cameras[0].getRay(points1)
         p2, v2 = self.cameras[1].getRay(points2)
-        return self.intersectionOfTwoLines(p1, v1, p2, v2)
-
-    def distanceOfTwoLines(self, p1, v1, p2, v2):
-        """
-        Get the point closest to the intersection of two lines. The lines are given by one point (p1 and p2) and a
-        direction vector v (v1 and v2). If a batch should be processed, the multiple direction vectors can be given.
-        """
-        # if we transform multiple points in one go
-        if len(v1.shape) == 2:
-            a1 = np.einsum('ij,ij->i', v1, v1)
-            a2 = np.einsum('ij,ij->i', v1, v2)
-            b1 = -np.einsum('ij,ij->i', v2, v1)
-            b2 = -np.einsum('ij,ij->i', v2, v2)
-            c1 = -np.einsum('ij,j->i', v1, p1 - p2)
-            c2 = -np.einsum('ij,j->i', v2, p1 - p2)
-            res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]).transpose(2, 0, 1), np.array([c1, c2]).T)
-            res = res[:, None, :]
-            #print("res[..., 0]", res[..., 0], res[..., 1], np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1))
-            return np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1)#+(res[..., 0]<10)*999+(res[..., 1]<10)*999
-        else:  # or just one point
-            a1 = np.dot(v1, v1)
-            a2 = np.dot(v1, v2)
-            b1 = -np.dot(v2, v1)
-            b2 = -np.dot(v2, v2)
-            c1 = -np.dot(v1, p1 - p2)
-            c2 = -np.dot(v2, p1 - p2)
-            res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]), np.array([c1, c2]))
-            res = res[None, None, :]
-            return np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1)[0]
+        return ray.intersectionOfTwoLines(p1, v1, p2, v2)
 
     def discanteBetweenRays(self, points1, points2):
         p1, v1 = self.cameras[0].getRay(points1, normed=True)
         p2, v2 = self.cameras[1].getRay(points2, normed=True)
-        return self.distanceOfTwoLines(p1, v1, p2, v2)
+        return ray.distanceOfTwoLines(p1, v1, p2, v2)
 
     def imagesFromSpace(self, points):
         return [cam.imageFromSpace(points) for cam in self.cameras]
--- CameraTransform/ray.py
+++ CameraTransform/ray.py
@@ -81,6 +81,62 @@ def ray_intersect_triangle(origin, direction, triangle, use_planes=False):
     return point
 
 
+def intersectionOfTwoLines(p1, v1, p2, v2):
+    """
+    Get the point closest to the intersection of two lines. The lines are given by one point (p1 and p2) and a
+    direction vector v (v1 and v2). If a batch should be processed, the multiple direction vectors can be given.
+    """
+    # if we transform multiple points in one go
+    if len(v1.shape) == 2:
+        a1 = np.einsum('ij,ij->i', v1, v1)
+        a2 = np.einsum('ij,ij->i', v1, v2)
+        b1 = -np.einsum('ij,ij->i', v2, v1)
+        b2 = -np.einsum('ij,ij->i', v2, v2)
+        c1 = -np.einsum('ij,j->i', v1, p1 - p2)
+        c2 = -np.einsum('ij,j->i', v2, p1 - p2)
+        res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]).transpose(2, 0, 1), np.array([c1, c2]).T)
+        res = res[:, None, :]
+        return np.mean([p1 + res[..., 0] * v1, p2 + res[..., 1] * v2], axis=0)
+    else:  # or just one point
+        a1 = np.dot(v1, v1)
+        a2 = np.dot(v1, v2)
+        b1 = -np.dot(v2, v1)
+        b2 = -np.dot(v2, v2)
+        c1 = -np.dot(v1, p1 - p2)
+        c2 = -np.dot(v2, p1 - p2)
+        res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]), np.array([c1, c2]))
+        res = res[None, None, :]
+        return np.mean([p1 + res[..., 0] * v1, p2 + res[..., 1] * v2], axis=0)[0]
+
+
+def distanceOfTwoLines(p1, v1, p2, v2):
+    """
+    Get the point closest to the intersection of two lines. The lines are given by one point (p1 and p2) and a
+    direction vector v (v1 and v2). If a batch should be processed, the multiple direction vectors can be given.
+    """
+    # if we transform multiple points in one go
+    if len(v1.shape) == 2:
+        a1 = np.einsum('ij,ij->i', v1, v1)
+        a2 = np.einsum('ij,ij->i', v1, v2)
+        b1 = -np.einsum('ij,ij->i', v2, v1)
+        b2 = -np.einsum('ij,ij->i', v2, v2)
+        c1 = -np.einsum('ij,j->i', v1, p1 - p2)
+        c2 = -np.einsum('ij,j->i', v2, p1 - p2)
+        res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]).transpose(2, 0, 1), np.array([c1, c2]).T)
+        res = res[:, None, :]
+        #print("res[..., 0]", res[..., 0], res[..., 1], np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1))
+        return np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1)#+(res[..., 0]<10)*999+(res[..., 1]<10)*999
+    else:  # or just one point
+        a1 = np.dot(v1, v1)
+        a2 = np.dot(v1, v2)
+        b1 = -np.dot(v2, v1)
+        b2 = -np.dot(v2, v2)
+        c1 = -np.dot(v1, p1 - p2)
+        c2 = -np.dot(v2, p1 - p2)
+        res = np.linalg.solve(np.array([[a1, b1], [a2, b2]]), np.array([c1, c2]))
+        res = res[None, None, :]
+        return np.linalg.norm((p1 + res[..., 0] * v1) - (p2 + res[..., 1] * v2), axis=1)[0]
+
 def extrudeLine(points, z0, z1):
     mesh = []
     last_point = None
